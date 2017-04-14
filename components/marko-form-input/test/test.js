91774d13f0f45785f3bb223bcab7e289660d30e4
Matteo Zambon <matteo@thetophat.org>
2017-04-14 00:00:21 -0300

    Improved ESLint, GIT Ignore Added NPM Ignore Added emply test file for `marko-form-input` Setup environment to accept `marko-starter` and `marko-test` (IDEA: make samples into the same repo)

---
+++ components/marko-form-input/test/test.js
@@ -0,0 +1,12 @@
+/*
+* @Author: Matteo Zambon
+* @Date:   2017-04-13 23:04:44
+* @Last Modified by:   Matteo Zambon
+* @Last Modified time: 2017-04-13 23:51:48
+*/
+
+'use strict'
+
+// const expect = require('chai').expect
+
+// TODO: write some tests for this component
